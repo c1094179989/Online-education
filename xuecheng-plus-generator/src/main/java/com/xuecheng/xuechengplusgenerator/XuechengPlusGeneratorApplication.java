dbe975f017e790678f4d114ba3193cf45980a12d
cgl <1094179989@qq.com>
2023-06-10 17:03:25 +0800

    导入mybatisplus自动生成器并生成8张表的数据库映射等文件

---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/xuechengplusgenerator/XuechengPlusGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.xuecheng.xuechengplusgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuechengPlusGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuechengPlusGeneratorApplication.class, args);
+    }
+
+}
