9cf86bbbfc722d567690177b27888dd66e2096f3
cgl <1094179989@qq.com>
2023-06-11 14:49:47 +0800

    内容管理po

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApplication.java
@@ -0,0 +1,19 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @className ContentApplication
+ * @description 内容管理服务启动类
+ * @author cgl
+ * @date 2023/06/11 10:55
+ * @version V1.0
+**/
+@SpringBootApplication
+public class ContentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContentApplication.class,args);
+    }
+
+}
